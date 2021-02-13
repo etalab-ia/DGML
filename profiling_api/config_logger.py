cd88463a3c73c587e9150888c9d4d8ecfc521c9a
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-02-13 18:55:46 +0100

    :sparkles: add pandas profiling api

---
+++ profiling_api/config_logger.py
@@ -0,0 +1,22 @@
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(asctime)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "api-logger": {"handlers": ["default"], "level": "DEBUG"},
+    },
+}
