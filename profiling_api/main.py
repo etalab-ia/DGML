5fef06175ed92b5f1444c0803c74dd5d342820be
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-02-15 15:59:36 +0100

    added root path and better error handling

--- profiling_api/main.py
+++ profiling_api/main.py
@@ -19,7 +19,7 @@ logging.config.dictConfig(log_config)
 logger = logging.getLogger("api-logger")
 resources_df: pd.DataFrame = pd.read_csv("./assets/dgf_resources.tar.gz", sep=";")
 
-app = FastAPI()
+app = FastAPI(root_path="/profiler")
 
 
 def get_profile(csv_path: Path, resource_metadata: Dict[str, str],
@@ -38,21 +38,14 @@ def get_profile(csv_path: Path, resource_metadata: Dict[str, str],
     return html_str
 
 
-def get_url(resource_id: str):
-    try:
-        resource_info = resources_df[resources_df.id == resource_id].to_dict("records")
-        return resource_info[0]
-    except IndexError as e:
-        logger.error(f"Could not find an url for this resource {resource_id}. Error: {e}")
-        raise e
+def get_resource_metadata(resource_id: str):
+    resource_info = resources_df[resources_df.id == resource_id].to_dict("records")
+    return resource_info[0]
 
 
 def download_file(resource_metadata: str):
-    try:
-        file_name, headers = urllib.request.urlretrieve(resource_metadata["url"])
-    except Exception as e:
-        logger.error(f"Could not download resource file. Error: {e}")
-        raise e
+    logger.info(f"Trying to download file {resource_metadata['url']}")
+    file_name, headers = urllib.request.urlretrieve(resource_metadata["url"])
     # TODO: check if download was successful
     return Path(file_name)
 
@@ -65,41 +58,44 @@ def get_csv_metadata(file_path: str):
 
 def get_report(resource_id: str, resource_url: str = None):
     cache = SqliteDict('./cache.sqlite', autocommit=True)
+    try:
 
-    # 1. Check if it is not in the cache
-    if resource_id in cache:
-        logger.info(f"Great! We found csv {resource_id} in cache")
-        return cache[resource_id]
+        # 1. Check if it is not in the cache
+        if resource_id in cache:
+            logger.info(f"Great! We found csv {resource_id} in cache")
+            return cache[resource_id]
 
-    # 2. It is not there. Let's get the download url
-    if not resource_url:
-        resource_metadata = get_url(resource_id=resource_id)
-        logger.info(f"Found the following info from the catalogue: {resource_metadata}")
-    else:
-        resource_metadata = {"id": resource_id, "resource_url": resource_url}
-        logger.warning(f"Did not found any info for resource {resource_id}")
+        # 2. It is not there. Let's get the download url
+        if not resource_url:
+            resource_metadata = get_resource_metadata(resource_id=resource_id)
+            logger.info(f"Found the following info from the catalogue: {resource_metadata}")
+        else:
+            resource_metadata = {"id": resource_id, "resource_url": resource_url}
+            logger.warning(f"Did not found any info for resource {resource_id}")
 
-    # 3. Try to download the csv
-    file_path = download_file(resource_metadata=resource_metadata)
+        # 3. Try to download the csv
+        file_path = download_file(resource_metadata=resource_metadata)
 
-    # 4. Get csv metadata
-    csv_detective_info = get_csv_metadata(file_path)
-    logger.info(f"Got csv-detective metadata")
-    # 5. Get and cache profile
-    html_report = get_profile(file_path, resource_metadata=resource_metadata,
-                              csv_metadata=csv_detective_info)
-    logger.info(f"Got csv profile")
+        # 4. Get csv metadata
+        csv_detective_info = get_csv_metadata(file_path)
+        logger.info(f"Got csv-detective metadata")
+        # 5. Get and cache profile
+        html_report = get_profile(file_path, resource_metadata=resource_metadata,
+                                  csv_metadata=csv_detective_info)
+        logger.info(f"Got csv profile")
 
-    # 6. Save report in cache
-    cache[resource_id] = html_report
-    logger.info(f"Saved csv profile in sqlite db")
+        # 6. Save report in cache
+        cache[resource_id] = html_report
+        logger.info(f"Saved csv profile in sqlite db")
+        return html_report
+    except Exception as e:
+        logger.error(f"Could not get pandas profiling of file {resource_id}. Error {e}")
+        return str(e)
+    finally:
+        cache.close()
 
-    cache.close()
 
-    return html_report
-
-
-@app.get("/profiler/{resource_id}", response_class=HTMLResponse)
+@app.get("/{resource_id}", response_class=HTMLResponse)
 def read_item(resource_id: str, resource_url: str = None):
     report_html = get_report(resource_id=resource_id, resource_url=resource_url)
     return report_html
