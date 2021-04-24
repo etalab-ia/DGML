ffbd8ab21deb3e76754577b326b440650ca0e2ae
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-04-24 23:52:57 +0200

    removed pprofiling api folder

--- profiling_api/run_docker.sh
+++
@@ -1,5 +0,0 @@
-echo "Build image"
-docker build -t pandas-profiling-api .
-
-echo "Run container"
-docker run -d -p 8080:80 -e MAX_WORKERS="2" -v /full/path/to/cache.sqlite:/app/cache.sqlite pandas-profiling-api
