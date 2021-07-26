45394499a7fa6522f7c5e1cdb1d53ef1d9544f81
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-07-26 17:29:02 +0200

    refactoring plus adding missing columns

---
+++ src/get_dataset.py
@@ -0,0 +1,142 @@
+from pathlib import Path
+
+import requests
+import pandas as pd
+import os
+
+
+def latest_catalog():
+    """This function returns the pandas dataframe of the latest version of dgf resource catalog
+    (https://www.data.gouv.fr/en/datasets/catalogue-des-donnees-de-data-gouv-fr/#_)"""
+    # dgf_catalog = 'https://www.data.gouv.fr/fr/datasets/r/4babf5f2-6a9c-45b5-9144-ca5eae6a7a6d'  # latest url of the catalog
+    dgf_catalog = './data/latest_dgf_catalog.zip'  # latest downloaded csv
+    dgf_catalog_df = pd.read_csv(dgf_catalog, delimiter=";", compression='zip', error_bad_lines=False)
+    pd.set_option('display.max_colwidth', None)  # to stop pandas from "cutting" long urls
+    return dgf_catalog_df
+
+
+def info_from_catalog(id: str, catalog: pd.DataFrame):
+    """This function returns a dictionary containing : the resource url, the resource format and the url of its data.gouv.fr page
+    --------------------------------------------
+    :param:     id: id of the dgf resource
+    :type:      id: string"""
+
+    url = catalog[catalog['id'] == id]['url']
+    if url.empty:
+        raise Exception(f"The dataset with id {id} was not found in DGF catalog")
+    url = url.values.item()
+
+    file_format = catalog[catalog['id'] == id]['format'].values.item()
+    dgf_page = catalog[catalog['id'] == id]['dataset.url'].values.item()
+    format_is_nan = catalog[catalog['id'] == id]['format'].isnull().values.any()
+    catalog_dict = {'url_resource': url, 'format': file_format, 'url_dgf': dgf_page, 'format_is_nan': format_is_nan}
+    return catalog_dict
+
+
+def is_referenced(url, id, catalog_info):
+    """Given the url of  a resource from the catalog, this function returns True if the resource is referenced by data.gouv.fr
+    False otherwise
+    :param      :url: url of a resource in the catalog
+    :type       :url: string"""
+    dgf_page = catalog_info['url_dgf']
+    headers = requests.head(url).headers
+    downloadable = 'attachment' in headers.get('Content-Disposition', '')
+    # download_zip = 'application/zip' in headers.get('Content-Type','')
+    if not downloadable:
+        # if not os.path.isfile(f'./datasets/resources/{id}/{id}.csv') :
+        if not Path().home().joinpath(f'open_ML/datasets/resources/{id}/{id}.csv'):
+            raise Exception(f'This id is associated to a dataset not referenced by data.gouv.fr. \n '
+                            f'Please download the dataset from here: {dgf_page}\n'
+                            f'Then manually upload it in the corresponding folder and name it: {id}.csv')
+    return downloadable
+
+
+def detect_csv(request):
+    """Given the url request for a csv or txt file, this function returns a dictionary containing the csv encoding and separator.
+    -------------------------------------------
+    :param        url: url containing the csv
+    :type         url: string"""
+    text = request.text[0:100]
+    encoding = request.encoding
+    if ";" in text:
+        sep = ";"
+    elif "," in text:
+        sep = ","
+    elif "|" in text:
+        sep = "|"
+    elif "\t" in text:
+        sep = "\t"
+    else:
+        raise TypeError('separator not detected')
+    url_dict = {'encoding': encoding, 'separator': sep}
+    return url_dict
+
+
+def load_dataset_without_url(id, ):
+    pass
+
+
+def load_dataset(id, catalog_info, output_dir=None):
+    """This function loads a csv in the datasets folder/creates a pandas dataframe given its id if the dataset is referenced by data.gouv.fr.
+    Otherwise, you get an error and you should manually upload it.
+    Remark: on data.gouv.fr, datasets are available in various "formats": json, shp, csv, zip, document, xls, pdf, html, xlsx,geojson etc.
+    to this day, our repository only contains files with .csv,.txt, .xls extensions, therefore we only treat these extensions.
+    -------------------
+    :param:     id: id of the dgf resource (must be a txt, csv or xls file)
+    :type:      id: string"""
+    url = catalog_info['url_resource']
+    referenced = is_referenced(url=url, id=id, catalog_info=catalog_info)
+    if referenced is True:  # if the dataset is referenced
+        file_format = catalog_info['format']
+        format_is_nan = catalog_info['format_is_nan']
+        request = requests.get(url)
+        delimiter = detect_csv(request)['separator']
+        encoding = detect_csv(request)['encoding']
+        if file_format == 'csv' or format_is_nan:  # if the format is not available on dgf, we assume it is a csv by default
+            if url.rsplit('.', 1)[-1] == 'zip':
+                dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='zip',
+                                        error_bad_lines=False)
+            elif url.rsplit('.', 1)[-1] == 'gz':
+                dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='gzip',
+                                        error_bad_lines=False)
+            elif url.rsplit('.', 1)[-1] == 'bz2':
+                dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='bz2',
+                                        error_bad_lines=False)
+            elif url.rsplit('.', 1)[-1] == 'xz':
+                dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='xz',
+                                        error_bad_lines=False)
+            else:
+                dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding)
+        elif file_format == 'txt':
+            dataframe = pd.read_table(url, sep=delimiter, encoding=encoding)
+        elif (file_format == 'xls') or (file_format == 'xlsx'):
+            dataframe = pd.read_excel(url, sheet_name=None)
+        elif file_format == 'zip':
+            dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='zip',
+                                    error_bad_lines=False)
+        elif file_format == 'gz':
+            dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='gzip',
+                                    error_bad_lines=False)
+        elif file_format == 'bz2':
+            dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='bz2',
+                                    error_bad_lines=False)
+        elif file_format == 'xz':
+            dataframe = pd.read_csv(url, sep=None, engine='python', encoding=encoding, compression='xz',
+                                    error_bad_lines=False)
+        else:
+            raise TypeError(
+                'Please choose a dataset that has one of the following extensions: .csv, .txt, .xls or choose '
+                'a compressed file having one of these extensions.')
+        return dataframe
+    else:
+        dataframe = pd.read_csv(Path().home().joinpath(f'open_ML/datasets/resources/{id}/{id}.csv'), sep=None,
+                                engine='python')
+        return dataframe
+
+
+def save_dataset(dataframe, id, output_dir):
+    dataframe.to_csv(output_dir.joinpath(f"{id}.csv"))
+
+# Remark on separators detection : the 'python engine' in pd.read_csv/read_table  works pretty well most of the time. However, it does not handle well some
+# exceptions (see for instance the dataset: 90a98de0-f562-4328-aa16-fe0dd1dca60f).
+# Improvements/to do: detect_csv:  separators detection should be handled better (not very robust, possibly does not cover all the exceptions)
---
+++ src/get_mljar.py
@@ -0,0 +1,114 @@
+import logging
+
+from app.apps.utils import slugify
+from src.get_statistic_summary import rejected_var, is_a_warning_col
+
+logger = logging.getLogger()
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from supervised.automl import AutoML
+
+
+# Minimal rules to be followed to get mljar working
+# 1. No nan in the target variable
+# 2. Drop columns with > 70%? of  missing values because useless
+# 3. Drop columns labeled as unsupported by Pandas Profiling and redundant columns
+# c. url and long texts are unsupported
+# d. drop columns with 100% of distinct values (such as IDs)
+
+
+def prepare_for_mljar(data, profiling, csv_data=None):
+    """This function returns a dataset properly prepared to be used by mljar
+    :param csv_data:
+    :param:     id: id of the dgf resource (must be a txt, csv or xls file)
+    :type:      id: string
+    :param:     target_variable: name of the target variable of the chosen ML task
+    :type:      target_variable: string
+    :param:     task: chosen ML task (regression, binary_classification, multi_classification
+    :type:      task: string
+    """
+    columns_to_drop = []
+    # 1. Check total number of NaN in dataset
+    if data.isna().sum().sum() / (len(data) * len(data.columns)) >= 30:
+        logger.warning('This dataset contains too many missing values. No ML task will be performed')
+        return
+
+    # 2. Remove pandas profiling rejected variables
+    columns_to_drop.extend(rejected_var(profiling))
+
+    # 3. Remove pandas profiling warning variables
+    columns_to_drop.extend([col for col in data.columns
+                            if is_a_warning_col(col, profiling=profiling)])
+
+    # 4. Remove variables with more than 70% of NaNs
+    columns_to_drop.extend([col for col in data.columns if
+                            data[col].isna().sum() / len(data) > 0.7])
+
+    # 5. Remove redundant variables
+    data = data.loc[:, ~data.columns.duplicated()]
+    description = profiling.get_description()
+    pandas_cat_col = [key for key, value in description['variables'].items() if
+                      str(value['type']) == 'Categorical']
+    pandas_cat_col = [col for col in pandas_cat_col if col in data.columns]
+    text_cols = [(data[col].str.split().str.len().mean(), col) for col in pandas_cat_col if
+                 np.any([isinstance(val, str) for val in data[col]])]
+    # 6. Remove long text columns
+    columns_to_drop.extend([text_cols[i][1] for i in range(len(text_cols)) if
+                            text_cols[i][0] > 15])
+
+    # 7. Remove url columns
+    columns_to_drop.extend([col for col in pandas_cat_col if (str(data[col][1]).startswith('https'))])
+
+    # 8. Remove csv-detective detected columns
+    if csv_data and "columns" in csv_data:
+        columns_to_drop.extend([slugify(col_name) for col_name, col_type in csv_data["columns"].items()
+                                if col_type not in ["booleen"]])
+
+    # Actually remove columns
+    data = data.drop(columns=set(columns_to_drop))
+    return data, columns_to_drop
+
+
+def generate_mljar(data, target_variable, output_dir, automl_mode):
+    """This function takes a properly prepared dataframe and performs AutoML on it. The generated output is the html report.
+    ------------------------------------------------
+    :param:     :data: dataframe on which we want to perform a given ML task
+    :type:      :data: pandas dataframe
+    :param:     :target_variable: chosen target_variable for the ML task
+    :type:      :target_variable: string
+    :param:     :output_dir: directory path for the mljar report
+    :type:      :output_dir: Path
+    :param:     :automl_mode: mode for AutoMl computing: choose 'Perform' to have more visualisations and features (but slower and not
+     always better performing; choose 'Explain' otherwise"""
+    y = data[target_variable].values
+    X = data.drop(columns=[target_variable])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    if automl_mode == 'Explain':
+        automl = AutoML(results_path=output_dir.as_posix(), total_time_limit=5 * 60, mode='Explain')
+        automl.fit(X_train, y_train)
+        automl.report()
+    else:
+        automl = AutoML(results_path=output_dir.as_posix(), total_time_limit=7 * 60, mode='Perform')
+        automl.fit(X_train, y_train)
+        automl.report()
+    return automl
+
+# def plot_mljar_table(id):
+#     """Returns a plot from the mljar leaderboard with train_time of the x-axis and metric_value on the y axis"""
+#     leaderboard = pd.read_csv(Path().home().joinpath(f'open_ML/datasets/resources/{id}/leaderboard.csv'))
+#     fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+#     leaderboard['train_time'] = leaderboard['train_time'].astype(float)
+#     leaderboard['metric_value'] = leaderboard['metric_value'].astype(float)
+#     ax.plot(leaderboard['train_time'], leaderboard['metric_value'], 'o')
+#     leaderboard['name'] = leaderboard['name'].astype('category')
+#     groups = leaderboard.groupby("name")
+#     for name, group in groups:
+#         plt.plot(group["train_time"], group["metric_value"], marker="o", linestyle="", label=name)
+#     for i, txt in enumerate(leaderboard['name']):
+#         ax.annotate(txt, (leaderboard['train_time'][i], leaderboard['metric_value'][i]), va='bottom')
+#     if leaderboard['metric_type'][1] == 'logloss':
+#         ax.set(xlabel='train_time (seconds)', ylabel='metric_value (logloss)')
+#     else:
+#         ax.set(xlabel='train_time(seconds)', ylabel='metric_value (rmse)')
+#     return figure
